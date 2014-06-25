0e108c5ac875b9cb849c14c2daff02d9be14a3a0
xhli110@126.com <xhli110@126.com>
2014-06-25 18:38:20 +0800

    调整结构

---
+++ src/main/java/com/afengzi/website/dao/group/group.java
@@ -0,0 +1,11 @@
+package com.afengzi.website.dao.group;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lixiuhai
+ * Date: 14-6-25
+ * Time: обнГ6:36
+ * To change this template use File | Settings | File Templates.
+ */
+public interface group {
+}
